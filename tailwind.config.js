69de09464da0b94d50cf9146b71728cf8799c69e
Luis <m.luishernandez@outlook.com>
2021-07-14 14:12:17 -0400

    Making a diferent approach with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      borderWidth:{
+        6:'6px'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
